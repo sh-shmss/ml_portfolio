726adc8144f1b2a684df20d731a675f99bcc5699
sh-shmss <sh.shmss@gmail.com>
2020-01-11 01:33:58 -0500

    database added

--- app.py
+++ app.py
@@ -9,18 +9,46 @@ from flask import Flask, request, jsonify, render_template, flash
 from forms.forms import PredictionForm, VisualizationForm
 from graph.graph import PlotNetwork
 from models.predictor import Predict
-#from bokeh.embed import components
-#from bokeh.plotting import figure
-
+import os
+from time import localtime, strftime
+import psycopg2
 from nltk.data import find
 import gensim
+
+secret_key = os.environ.get('SECRET_KEY')
+db_username = os.environ.get('DB_USERNAME')
+db_password = os.environ.get('DB_PASSWORD')
+
+
 word2vec_sample = str(find('models/word2vec_sample/pruned.word2vec.txt'))
 model = gensim.models.KeyedVectors.load_word2vec_format(
     word2vec_sample, binary=False)
 
 
+def insert_db(component, user_input, user_input_no, prediction):
+    ip = request.remote_addr
+    timestamp = strftime("%Y-%m-%d %H:%M:%S", localtime()) 
+    connection = psycopg2.connect(user = db_username,
+                                  password = db_password,
+                                  host = "127.0.0.1",
+                                  port = "5432",
+                                  database = "ml")
+
+    cursor = connection.cursor()
+
+    postgres_insert_query = """ INSERT INTO ml(component, user_input, user_input_no, prediction, timestamp, ip) VALUES (%s,%s,%s,%s,%s,%s)"""
+    record_to_insert = (component, user_input, user_input_no, prediction, timestamp, ip)
+    cursor.execute(postgres_insert_query, record_to_insert)
+
+    connection.commit()
+    count = cursor.rowcount
+
+    cursor.close()
+    connection.close()
+
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SECRET_KEY'] = secret_key
 app.debug = True
 
 
@@ -37,6 +65,7 @@ def language_detection():
     if form.user_input.data:
         text = form.user_input.data
         prediction = Predict([text]).detect_language()
+        insert_db("ld", text, str(0), str(prediction))
     return render_template('ld.html', prediction=prediction, form=form)
 
 
@@ -47,6 +76,7 @@ def sentiment_analysis():
     if form.user_input.data:
         text = form.user_input.data
         prediction = Predict(text).analyze_sentiment()
+        insert_db("sa", text, str(0), str(prediction))
     return render_template('sa.html', prediction=prediction, form=form)
 
 
@@ -60,8 +90,11 @@ def data_visualization():
         current_word, current_no = 'data', 50
     plot = PlotNetwork(current_word, int(current_no), model).make_plot()
     script, div = components(plot)
+    insert_db("dv", str(current_word), str(current_no), "none")
     return render_template("dv.html", form=form, script=script, div=div)
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
+
+
--- forms/forms.py
+++ forms/forms.py
@@ -1,14 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField, TextField, SubmitField
-from wtforms.validators import Required
+from wtforms.validators import Required,NumberRange
 
 
 class PredictionForm(FlaskForm):
-    user_input = TextAreaField()
+    user_input = TextAreaField(validators=[Required()])
     submit = SubmitField('Predict')
 
 
 class VisualizationForm(FlaskForm):
-    user_input = TextField()
-    user_input_no = TextField()
+    user_input = TextField(validators=[Required()])
+    user_input_no = TextField(validators=[Required(),NumberRange(min=1)])
     submit = SubmitField('Visualize')
